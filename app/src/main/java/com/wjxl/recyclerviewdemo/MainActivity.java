d7a6126fcc7bf418ad3f611f704e573b46fa222b
YuGoal <1127caoyu@gmail.com>
2016-06-14 17:23:35 +0800

    Initial commit

---
+++ app/src/main/java/com/wjxl/recyclerviewdemo/MainActivity.java
@@ -0,0 +1,112 @@
+package com.wjxl.recyclerviewdemo;
+
+import android.graphics.Rect;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView mRecyclerView;
+    private ArrayList<String> mData = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerview);
+
+        for(int i = 0;i<20;i++){
+            mData.add("a"+i);
+        }
+
+        final HomeAdapter adapter = new HomeAdapter();
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this));
+        //设置adapter
+        mRecyclerView.setAdapter(adapter);
+        //设置Item增加、移除动画
+        mRecyclerView.setItemAnimator(new DefaultItemAnimator());
+
+        //滑动回调
+        ItemTouchHelper.Callback mCallback = new ItemTouchHelper.SimpleCallback(ItemTouchHelper.UP|ItemTouchHelper.DOWN,ItemTouchHelper.RIGHT) {
+            /**
+             * @param recyclerView
+             * @param viewHolder 拖动的ViewHolder
+             * @param target 目标位置的ViewHolder
+             * @return
+             */
+            @Override
+            public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
+                int fromPosition = viewHolder.getAdapterPosition();//得到拖动ViewHolder的position
+                int toPosition = target.getAdapterPosition();//得到目标ViewHolder的position
+                if (fromPosition < toPosition) {
+                    //分别把中间所有的item的位置重新交换
+                    for (int i = fromPosition; i < toPosition; i++) {
+                        Collections.swap(mData, i, i + 1);
+                    }
+                } else {
+                    for (int i = fromPosition; i > toPosition; i--) {
+                        Collections.swap(mData, i, i - 1);
+                    }
+                }
+                adapter.notifyItemMoved(fromPosition, toPosition);
+                //返回true表示执行拖动
+                return true;
+            }
+
+            @Override
+            public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
+                int position = viewHolder.getAdapterPosition();
+                mData.remove(position);
+                adapter.notifyItemRemoved(position);
+            }
+
+        };
+        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(mCallback);
+        itemTouchHelper.attachToRecyclerView(mRecyclerView);
+
+    }
+
+
+    //适配器
+    class HomeAdapter extends RecyclerView.Adapter<MyViewHolder>{
+
+        @Override
+        public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            MyViewHolder holder = new MyViewHolder(LayoutInflater.from(
+                    MainActivity.this).inflate(R.layout.item, parent,
+                    false));
+            return holder;
+        }
+
+        @Override
+        public void onBindViewHolder(MyViewHolder holder, int position) {
+            holder.tv.setText(mData.get(position));
+        }
+
+
+        @Override
+        public int getItemCount() {
+            return mData.size();
+        }
+
+    }
+    class MyViewHolder extends RecyclerView.ViewHolder{
+        TextView tv;
+        public MyViewHolder(View itemView) {
+            super(itemView);
+            tv = (TextView) itemView.findViewById(R.id.tv_num);
+        }
+    }
+
+}
